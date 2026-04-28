534b8b6e1e3cc410f068559984257b0e603aac05
Karen <A23216367@alumnos.uady.mx>
2026-04-27 18:01:28 -0600

    uibuild

--- part3/Phonebook_backend/index.js
+++ part3/Phonebook_backend/index.js
@@ -3,6 +3,7 @@ const morgan = require('morgan')
 const app = express()
 
 
+app.use(express.static('dist'))
 
 app.use(express.json())
 
@@ -100,7 +101,7 @@ app.post('/api/persons', (request, response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on por ${PORT}`)
 })
